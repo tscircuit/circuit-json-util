43e21bde96a4abbe7ac3f37426d73d92a76d8adf
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-07-27 10:07:13 +0530

    `source_group` of the board is also added to the tree when no nested group is present (#47)
    * source_group of the board is also added to the tree when no nested group is present
    
    * remove comments

---
+++ tests/getCircuitJsonTreeBoardRoot.test.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "bun:test"
+import { getCircuitJsonTree } from "../lib/getCircuitJsonTree"
+import { runTscircuitCode } from "tscircuit"
+import { getStringFromCircuitJsonTree } from "lib/getStringFromCircuitJsonTree"
+
+test("getCircuitJsonTree - board only includes root source_group", async () => {
+  const circuitJson = await runTscircuitCode(`
+
+  export default () => (
+    <board autoroutingDisabled>
+      <resistor name="R1" resistance="1k" />
+      <capacitor name="C1" capacitance="100nF" />
+    </board>
+  )
+  `)
+
+  const tree = getCircuitJsonTree(circuitJson)
+
+  expect(getStringFromCircuitJsonTree(tree)).toMatchInlineSnapshot(`
+    "source_group_0
+      R1
+      C1"
+  `)
+}) 
